b7c2cccf393c5482029f2a8510170f39c2cb9cb5
RussianJeb <russianjeb@protonmail.com>
2020-08-25 10:38:46 +0300

    Add CORS

---
+++ src/main/java/ru/jeb/todolist/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ru.jeb.todolist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Jeb
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
+    }
+}
